46f6a369494791dc1558676a9980972250afd914
Adalberto Silveira  Napoles <asilveira@3htp.com>
2023-10-23 11:14:07 -0500

    Adjusting SQL in IIncidentDAO "Insidentes por quebrar"
    Function: findPaginatedIncidentsSlaByFilters

--- src/main/java/com/logicalis/apisolver/dao/IIncidentDAO.java
+++ src/main/java/com/logicalis/apisolver/dao/IIncidentDAO.java
@@ -92,88 +92,84 @@ public interface IIncidentDAO extends PagingAndSortingRepository<Incident, Long>
             "                     AND (a.created_on BETWEEN TO_DATE(?17,'YYYY-MM-DD') \n" +
             "                               AND TO_DATE(?18,'YYYY-MM-DD'))))\n" +
             "         AND a.delete IS FALSE",nativeQuery = true)
+
     public Page<IncidentFields> findPaginatedIncidentsByFilters(Pageable pageRequest, String filter, Long assignedTo, Long company, String state, boolean openUnassigned, boolean solved, String incidentParent, String scRequestParent, String scRequestItemParent, Long assignedToGroup, boolean closed, boolean open, List<Long> sysGroups, List<Long> sysUsers, List<String> states, List<String> priorities, String createdOnFrom, String createdOnTo);
 
-    @Query(value = "SELECT a.id AS id, \n" +
-            "             a.number AS number, \n" +
-            "             a.short_description AS short_description, \n" +
-            "             d.label AS state, \n" +
-            "             a.category AS category, \n" +
-            "             a.subcategory AS subcategory, \n" +
-            "             a.active AS active, \n" +
-            "             e.label AS priority, \n" +
-            "             a.ambite_level AS ambite_level, \n" +
-            "             a.platform_level AS platform_level, \n" +
-            "             a.service_level AS service_level, \n" +
-            "             a.specification_level AS specification_level, \n" +
-            "             b.name AS assignment_group, \n" +
-            "             j.name AS caller, \n" +
-            "             TO_CHAR(a.created_on, 'DD-MM-YYYY HH24:MI:SS') AS created_on, \n" +
-            "             TO_CHAR(a.updated_on, 'DD-MM-YYYY HH24:MI:SS') AS updated_on, \n" +
-            "             h.name AS assigned_to, \n" +
-            "             COALESCE(f.vip, false) AS vip, \n" +
-            "             a.incident_parent AS incident_parent, \n" +
-            "             a.count_parent AS count_parent, \n" +
-            "             f.user_type AS user_type, \n" +
-            "             COALESCE(i.number,'') as parent \n" +
-            "FROM incident a \n" +
-            "    INNER JOIN sys_group b ON a.assignment_group = b.id \n" +
-            "    INNER JOIN choice d ON a.state = d.value AND d.NAME = 'incident' AND d.element = 'state' \n" +
-            "    LEFT OUTER JOIN choice e ON a.priority = e.value AND e.NAME = 'task' AND e.element = 'priority' \n" +
-            "    LEFT OUTER JOIN sys_user f ON a.task_for = f.id \n" +
-            "    LEFT OUTER JOIN sys_user h ON a.assigned_to = h.id \n" +
-            "    LEFT OUTER JOIN incident i ON COALESCE(a.incident_parent, '') = i.integration_id \n" +
-            "    LEFT OUTER JOIN sys_user j ON a.caller = j.id \n" +
-            "    INNER JOIN (SELECT DISTINCT a.id AS incident, b.stage \n" +
-            "                FROM   incident a \n" +
-            "                    INNER JOIN task_sla b ON a.id = b.incident \n" +
-            "                WHERE  stage = 'in_progress' \n" +
-            "                AND b.percentage IS NOT NULL \n" +
-            "                AND b.percentage != '' \n" +
-            "                AND Cast(Split_part(COALESCE(b.percentage, '0'), '.', 1) AS INTEGER) > 90)  g ON coalesce(g.incident, 0) = a.id \n" +
-            "WHERE (?1 = '' OR UPPER(a.number                     \n" +
-            "             || coalesce(a.short_description, '') \n" +
-            "             || coalesce(a.category, '') \n" +
-            "             || coalesce(d.label, '') \n" +
-            "             || coalesce(a.subcategory, '') \n" +
-            "             || coalesce(e.label, '') \n" +
-            "             || coalesce(a.ambite_level, '') \n" +
-            "             || coalesce(a.platform_level, '') \n" +
-            "             || coalesce(a.service_level, '') \n" +
-            "             || coalesce(a.specification_level, '') \n" +
-            "             || coalesce(b.name, '') \n" +
-            "             || coalesce(h.name, '')   \n" +
-            "             || a.integration_id ) like ?1)         \n" +
-            "             AND (?2 = 0 OR a.assigned_to = ?2) \n" +
-            "             AND (?3 = 0 OR a.company = ?3)   \n" +
-            "             AND (?4 = '' OR a.state = ?4)  \n" +
-            "             AND (?5 = false OR (a.assigned_to IS null AND UPPER(d.label) NOT LIKE '%CERRADO%' AND UPPER(d.label) NOT LIKE '%CANCELADO%')) \n" +
-            "             AND (?6 = '' OR a.incident_parent = ?6)   \n" +
-            "             AND (?7= '' OR a.sc_request_parent = ?7)   \n" +
-            "             AND (?8 = '' OR a.sc_request_item_parent = ?8)  \n" +
-            "             AND ((?9 = 0) or exists ( select 'x'    \n" +
-            "                                                   from sys_group      b \n" +
-            "                                                      INNER JOIN sys_user_group c ON c.sys_group = b.id  \n" +
-            "                                                   where  b.id = a.assignment_group \n" +
-            "                                                   and    b.active is TRUE \n" +
-            "                                                   and    c.sys_user = ?9) )  \n" +
-            "             AND ((COALESCE(?10) IS NULL) OR a.assignment_group IN (?10)) \n" +
-            "             AND ((COALESCE(?11) IS NULL) OR a.assigned_to IN (?11) ) \n" +
-            "             AND ((COALESCE(?12) IS NULL) OR UPPER(d.label) IN (?12))  \n" +
-            "             AND ((COALESCE(?13) IS NULL) OR UPPER(e.label) IN (?13))  \n" +
-            "              AND ((?14 = '' AND ?15 = '' ) \n" +
-            "                    OR (?14 != '' AND ?15 = '' \n" +
-            "                      AND (a.created_on BETWEEN TO_DATE(?14,'YYYY-MM-DD') \n" +
-            "                              AND CURRENT_DATE)  ) \n" +
-            "                    OR (?14 = '' AND ?15 != ''  \n" +
-            "                       AND (a.created_on BETWEEN TO_DATE(('1900-01-01'),'YYYY-MM-DD') \n" +
-            "                                 AND TO_DATE(?15,'YYYY-MM-DD'))) \n" +
-            "                    OR (?14 != '' AND ?15 != '' \n" +
-            "                       AND (a.created_on BETWEEN TO_DATE(?14,'YYYY-MM-DD') \n" +
-            "                                AND TO_DATE(?15,'YYYY-MM-DD')))) \n" +
-            "             AND upper(d.label) NOT LIKE '%CERRADO%' \n" +
-            "             AND upper(d.label) NOT LIKE '%CANCELADO%' \n" +
-            "             AND b.active IS TRUE ",nativeQuery = true)
+    @Query(value = "SELECT a.id AS id,  \n" +
+            "    a.number AS number,  \n" +
+            "    a.short_description AS short_description,  \n" +
+            "                           d.label AS state,  \n" +
+            "                           a.category AS category,  \n" +
+            "                           a.subcategory AS subcategory,  \n" +
+            "                           a.active AS active,  \n" +
+            "                           e.label AS priority,  \n" +
+            "                           a.ambite_level AS ambite_level,  \n" +
+            "                           a.platform_level AS platform_level,  \n" +
+            "                           a.service_level AS service_level,  \n" +
+            "                           a.specification_level AS specification_level,  \n" +
+            "                           b.name AS assignment_group,  \n" +
+            "                           j.name AS caller,  \n" +
+            "                           TO_CHAR(a.created_on, 'DD-MM-YYYY HH24:MI:SS') AS created_on,  \n" +
+            "                           TO_CHAR(a.updated_on, 'DD-MM-YYYY HH24:MI:SS') AS updated_on,  \n" +
+            "                           h.name AS assigned_to,  \n" +
+            "                           COALESCE(f.vip, false) AS vip,  \n" +
+            "                           a.incident_parent AS incident_parent,  \n" +
+            "                           a.count_parent AS count_parent,  \n" +
+            "                           f.user_type AS user_type,  \n" +
+            "                           COALESCE(i.number,'') as parent  \n" +
+            "              FROM incident a  \n" +
+            "                  INNER JOIN sys_group b ON a.assignment_group = b.id  \n" +
+            "                  INNER JOIN choice d ON a.state = d.value AND d.NAME = 'incident' AND d.element = 'state'  \n" +
+            "                  LEFT OUTER JOIN choice e ON a.priority = e.value AND e.NAME = 'task' AND e.element = 'priority'  \n" +
+            "                  LEFT OUTER JOIN sys_user f ON a.task_for = f.id  \n" +
+            "                  LEFT OUTER JOIN sys_user h ON a.assigned_to = h.id  \n" +
+            "                  LEFT OUTER JOIN incident i ON COALESCE(a.incident_parent, '') = i.integration_id  \n" +
+            "                  LEFT OUTER JOIN sys_user j ON a.caller = j.id                                \n" +
+            "              WHERE (?1 = '' OR \n" +
+            "                      UPPER(a.number ) \n" +
+            "                      ||coalesce(a.short_description, '')  \n" +
+            "                            ||coalesce(a.category, '')  \n" +
+            "                            ||coalesce(d.label, '')  \n" +
+            "                            ||coalesce(a.subcategory, '')  \n" +
+            "                            ||coalesce(e.label, '')  \n" +
+            "                            ||coalesce(a.ambite_level, '')  \n" +
+            "                            ||coalesce(a.platform_level, '')  \n" +
+            "                            ||coalesce(a.service_level, '')  \n" +
+            "                            ||coalesce(a.specification_level, '')  \n" +
+            "                            ||coalesce(b.name, '')  \n" +
+            "                            ||coalesce(h.name, '')    \n" +
+            "                            ||a.integration_id  like ?1)         \n" +
+            "               AND (?2 = 0 OR a.assigned_to = ?2) \n" +
+            "               AND (?3 = 0 OR a.company = ?3) \n" +
+            "               AND (?4 = '' OR a.state = ?4)   \n" +
+            "               AND (?5 = false \n" +
+            "                          OR (a.assigned_to IS null AND UPPER(d.label)  NOT LIKE ALL (ARRAY['%CERRADO%','%CANCELADO%'] ))  )\n" +
+            "               AND (?6 = '' OR a.incident_parent = ?6)  \n" +
+            "               AND (?7 = '' OR a.sc_request_parent = ?7) \n" +
+            "               AND (?8 = '' OR a.sc_request_item_parent = ?8)  \n" +
+            "               AND ((?9 = 0 and exists ( select  'x' from sys_user_group c where  c.sys_group = a.assignment_group \n" +
+            "                               and c.active is true)) \n" +
+            "                           or exists ( select 'x'   \n" +
+            "                                       from   vw_user b   \n" +
+            "                                       where  b.id  = a.assignment_group  \n" +
+            "                                       AND    b.sys_user = ?9  \n" +
+            "                                       and    b.company = a.company) )    \n" +
+            "               and exists ( select 'x' from task_sla b \n" +
+            "                      WHERE  b.incident  = a.id        and    b.stage = 'in_progress' \n" +
+            "                      AND    b.percentage IS NOT NULL  AND    b.percentage != '' \n" +
+            "                      AND    Cast(Split_part(COALESCE(b.percentage, '0'), '.', 1) AS INTEGER) > 90  )\n" +
+            "               and  UPPER(d.label) NOT LIKE ALL (ARRAY['%CERRADO%','%CANCELADO%'])  \n" +
+            "               AND ((COALESCE(?10) IS NULL) OR a.assignment_group IN (?10)) \n" +
+            "               AND ((COALESCE(?11) IS NULL) OR a.assigned_to IN  (?11) )  \n" +
+            "               AND ((COALESCE(?12) IS NULL) OR UPPER(d.label) IN (?12))  \n" +
+            "               AND ((COALESCE(?13) IS NULL) OR UPPER(e.label) IN (?13))  \n" +
+            "               AND ((?14 = '' AND ?15 = '' ) \n" +
+            "                        OR (?14 != '' AND ?15 = ''  \n" +
+            "                           AND (a.created_on BETWEEN TO_DATE(?14,'YYYY-MM-DD')  AND CURRENT_DATE)  )  \n" +
+            "                        OR (?14 = '' AND ?15 != ''  AND (a.created_on BETWEEN TO_DATE(('1900-01-01'),'YYYY-MM-DD')  \n" +
+            "                           AND TO_DATE(?15,'YYYY-MM-DD')))  \n" +
+            "                        OR (?14 != '' AND ?15 != '' AND (a.created_on BETWEEN TO_DATE(?14,'YYYY-MM-DD')  \n" +
+            "                           AND TO_DATE(?15,'YYYY-MM-DD'))))",nativeQuery = true)
     public Page<IncidentFields> findPaginatedIncidentsSlaByFilters(Pageable pageRequest, String filter, Long assignedTo, Long company, String state, boolean openUnassigned, String incidentParent, String scRequestParent, String scRequestItemParent, Long assignedToGroup, List<Long> sysGroups, List<Long> sysUsers, List<String> states, List<String> priorities, String createdOnFrom, String createdOnTo);
 
     @Query(value = "SELECT DISTINCT a.id AS id,\n" +
